54e560a33d48000368e64a28cbe9dc754f163a82
Alexander <alromanov65@yandex.ru>
2020-06-14 22:50:47 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+/*const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers); */
+
+/*
+// Переменные
+// "use strict";
+
+// a = 15;
+// console.log(a);
+
+// let number = 5;
+// const pia = 1;
+
+// number = 10;
+// console.log(number);
+
+// var name = 'Ivan';
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+// Эксперименты с выводом массива по ID на страницу
+// window.onload = function () {
+//     let arr = [],
+//         i, j
+//     let out_arr = document.getElementById("out_arr")
+//     for (let i = 0; i <= 3; i++) {
+
+//         arr[i] = []
+//         for (let j = 0; j <= 3; j++) {
+
+//             arr[i][j] = j + 1
+//         }
+//     }
+// }
+
+//Эксперименты с выводом массива
+// out_arr.innerHTML = arr;
+// }
+// let arr = [], i, j
+// for (let i = 0; i <= 3; i++)
+// {
+//     arr[i] = []
+//     for (let j = 0; j <= 3; j++)
+//     {
+//         arr[i][j] = j+1   
+//     }
+// }
+// console.log(arr)
+
+
+//Эксперименты с объектом
+// const person = {
+//     firstName: 'Alex',
+//     lastName: 'Grey',
+//     myChilds: ['Kate','Pavel','Felix']
+// }
+//const arr = ['Kate', 'Pavel', 'Felix']
+//arr[arr.length] = prompt()
+//alert(arr)
+// alert(person.myChilds = prompt())
+//person.myChilds[myChilds.length] = 'd';
+//alert(person.myChilds)
+*/
