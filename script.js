df2a5e9efd24f252a5b2639d6c1b3f4573752ca5
Alexander <alromanov65@yandex.ru>
2020-06-15 23:11:55 +1100

    Practice task complete

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 "use strict";
 
-let incr = 10,
-    decr = 10;
+//Решение задачи по уроку
 
-incr++;
-decr--;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 
-console.log(incr);
-console.log(decr);
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+/*
+//Самостоятельное решение задачи
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+let lastWatchedFilm = prompt("Один из последних просмотренных фильмов?");
+let filmRating = prompt("На сколько оцените его?");
+
+let personalMovieDB = {
+        count: numberOfFilms,
+        movies: {
+            lastWatchedFilm,
+            filmRating
+        },
+        actors: {},
+        genres: [],
+        privat: false
+};
+
+alert("Кол-во просмотренных фильмов: " + personalMovieDB.count + "\n" + " Последний фильм: " + personalMovieDB.movies.lastWatchedFilm + "\n" + " Оценка: " + personalMovieDB.movies.filmRating);
+*/
 
 /*const answers = [];
 
